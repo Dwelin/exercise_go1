672cb8ad8bcd46b64e262e0f90cb564b5201418f
Dwelin <rehun@foxmail.com>
2022-08-10 10:48:09 +0800

    重构中间件

--- main.go
+++ main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"database/sql"
+	"goblog/app/middlewares"
 	"goblog/bootstrap"
-	"goblog/pkg/database"
+	"goblog/pkg/logger"
 	"net/http"
-	"strings"
-
-	"github.com/gorilla/mux"
 )
 
 type Result interface {
@@ -15,36 +12,12 @@ type Result interface {
 	RowsAffected() (int64, error) // 表示影响的数据表行数
 }
 
-var router *mux.Router
-var db *sql.DB
-
-func forceHTMLMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// 设置标头
-		w.Header().Set("Content-type", "text/html; charset=utf-8")
-		// 继续处理请求
-		next.ServeHTTP(w, r)
-	})
-}
-
-func removeTrailingSlash(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/" {
-			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
-		}
-		next.ServeHTTP(w, r)
-	})
-}
-
 func main() {
 
-	database.Initialize()
-	db = database.DB
+	bootstrap.SetupDB()
 
-	router = bootstrap.SetupRoute()
+	router := bootstrap.SetupRoute()
 
-	// 中间件：强制内容类型为 HTML
-	router.Use(forceHTMLMiddleware)
-
-	http.ListenAndServe(":3000", removeTrailingSlash(router))
+	err := http.ListenAndServe(":3000", middlewares.RemoveTrailingSlash(router))
+	logger.LogError(err)
 }
