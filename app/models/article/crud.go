ddb54402aa0b996b0b8bfe3928fd729191c1c2c4
Dwelin <rehun@foxmail.com>
2022-08-13 15:55:50 +0800

    创建用户

--- app/models/article/crud.go
+++ app/models/article/crud.go
@@ -10,7 +10,7 @@ import (
 func Get(idstr string) (Article, error) {
 	var article Article
 	id := types.StringToUint64(idstr)
-	if err := model.ConnectDB().First(&article, id).Error; err != nil {
+	if err := model.DB.First(&article, id).Error; err != nil {
 		return article, err
 	}
 	return article, nil
@@ -19,7 +19,7 @@ func Get(idstr string) (Article, error) {
 // GetAll 获取全部文章
 func GetAll() ([]Article, error) {
 	var articles []Article
-	if err := model.ConnectDB().Find(&articles).Error; err != nil {
+	if err := model.DB.Find(&articles).Error; err != nil {
 		return articles, err
 	}
 	return articles, nil
@@ -27,7 +27,7 @@ func GetAll() ([]Article, error) {
 
 // Create 创建文章，通过 article.ID 来判断是否创建成功
 func (article *Article) Create() (err error) {
-	result := model.ConnectDB().Create(&article)
+	result := model.DB.Create(&article)
 	if err = result.Error; err != nil {
 		logger.LogError(err)
 		return err
@@ -37,7 +37,7 @@ func (article *Article) Create() (err error) {
 
 // Update 更新文章
 func (article *Article) Update() (rowsAffected int64, err error) {
-	result := model.ConnectDB().Save(&article)
+	result := model.DB.Save(&article)
 	if err = result.Error; err != nil {
 		logger.LogError(err)
 		return 0, err
@@ -47,7 +47,7 @@ func (article *Article) Update() (rowsAffected int64, err error) {
 
 // Delete 删除文章
 func (article *Article) Delete() (rowsAffected int64, err error) {
-	result := model.ConnectDB().Delete(&article)
+	result := model.DB.Delete(&article)
 	if err = result.Error; err != nil {
 		logger.LogError(err)
 		return 0, err
